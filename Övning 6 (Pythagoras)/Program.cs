8974bea4ff25ed6226935204b749b00e79842ff1
Aron <aronmn@vgy.se>
2017-01-27 17:31:25 +0100

    Övning 6

--- Övning 6 (Pythagoras)/Program.cs
+++ Övning 6 (Pythagoras)/Program.cs
@@ -9,7 +9,20 @@ namespace Övning_6__Pythagoras_
     class Program
     {
         static void Main(string[] args)
-        {
+        { int a=0, b=0, c = 0;
+            for (a = 0; a<=1000; a++)
+            {
+                for (b=0; b<=1000; b++)
+                {
+                    for (c=0; c<=1000; c++)
+                    {
+                        if ((a * a + b * b == c * c && ((a + b + c) == 1000) && a<b && b<c))
+                        {
+                            Console.WriteLine("a är: " + a + "     b är : " + b + "     c är : " + c );
+                        }
+                    }
+                }
+            }
         }
     }
 }
