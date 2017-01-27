864675dba5b6cc13a97c26852ccc8c18fc563b21
Aron <aronmn@vgy.se>
2017-01-27 14:18:56 +0100

    Add project files.

---
+++ Övning 6 (Pythagoras)/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Övning_6__Pythagoras_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
